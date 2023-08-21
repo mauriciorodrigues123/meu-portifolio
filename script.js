6d183231dfe0c156f4c0d9d87af89dbdb8b5a420
Mauricio rodrigues bispo <125503314+mauriciorodrigues123@users.noreply.github.com>
2023-08-21 11:28:52 -0300

    subir os arquivos do meu site

---
+++ script.js
@@ -0,0 +1,36 @@
+var p =document.getElementById('p')
+
+function entrar() {
+    p.innerText = 'HTML É UMA LINGUAGEM\n DE MARCAÇÃO ULTILAZADA\n NA CONSTRUÇÃO DE PÁGINAS NA WEB'
+}
+
+function sair() {
+    p.innerText ='/*passe o mouse\n ou o dedo por cima\n de alguma habilidade\n para ler a descrição */'
+}
+
+function entraar() {
+    p.innerText = 'CSS É UMA LINGUAGEM\n DE ESTILIZAÇÃO PARA ADICIONAR\n ESTILO A UM DOCUMENTO'
+}
+
+function saair() {
+    p.innerText ='/*passe o mouse\n ou o dedo por cima\n de alguma habilidade\n para ler a descrição */'
+}
+
+function entraaar() {
+    p.innerText = 'JAVASCRIPT É UMA LINGUAGEM DE\n PROGRAMÇÃO. E JUTAMENTE COM\n HTML E CSS É UMA DAS TRÊS PRINCIPAIS\n TECNOLOGIAS DA WEB'
+}
+
+function saaair() {
+    p.innerText ='/*passe o mouse\n ou o dedo por cima\n de alguma habilidade\n para ler a descrição */'
+}
+
+function entraaarr() {
+    p.innerText = 'BOOTSTRAP É UM Framework web para\n design responsivo'
+}
+
+function saaairr() {
+    p.innerText ='/*passe o mouse\n ou o dedo por cima\n de alguma habilidade\n para ler a descrição */'
+}
+
+
+
